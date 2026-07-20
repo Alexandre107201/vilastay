fb3100b48c645419017ef02f9882700948d26ca2
Alexandre107201 <manuelalexandreadm@yahoo.com.br>
2026-07-20 10:18:31 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -9,7 +9,7 @@
 // ============================================================
 
 window.VILASTAY_CONFIG = {
-  SUPABASE_URL: "COLE_AQUI_A_URL_DO_PROJETO",
-  SUPABASE_ANON_KEY: "COLE_AQUI_A_CHAVE_ANON_PUBLIC",
+  SUPABASE_URL: "https://gvexbpqibgwjguntrmni.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2ZXhicHFpYmd3amd1bnRybW5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0ODg1MTksImV4cCI6MjEwMDA2NDUxOX0.ktRswgnsigM5p971gaLInUVfMlRp1R9LYw_X5tLHYtI",
   BRAND: "Vila Stay",
 };
